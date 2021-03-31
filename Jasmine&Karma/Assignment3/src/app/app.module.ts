270efe5104e414efc300a330165ea52a361bcdf3
kuldip.ladola <kuldip.ladola@DSK-158>
2021-03-31 17:28:45 +0530

    Angular Assignment 3 Changes done

--- Jasmine&Karma/Assignment3/src/app/app.module.ts
+++ Jasmine&Karma/Assignment3/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LightswitchComponent } from './lightswitch/lightswitch.component';
+import { CounterComponent } from './counter/counter.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CheckNumberComponent } from './check-number/check-number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LightswitchComponent,
+    CounterComponent,
+    CalculatorComponent,
+    CheckNumberComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
